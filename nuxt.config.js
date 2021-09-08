ffa4b34fec5fbe2e021ffa503f936e0bfd54d001
Sugar_F0x <sugarfox.dev@gmail.com>
2021-09-08 22:53:16 +0300

    :milky_way: Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+import colors from 'vuetify/es5/util/colors'
+
+const TITLE = 'ОМТУ Календарь'
+const DESCRIPTION = 'Календарь расписания занятий ОМТУ'
+const URL = 'https://omtu.sugarfox.ru'
+
+export default {
+  ssr: false,
+  target: 'static',
+  htmlAttrs: {
+    lang: 'ru'
+  },
+  head: {
+    title: TITLE,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: DESCRIPTION },
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'og:url', content: URL },
+      { name: 'og:title', content: TITLE },
+      { name: 'og:description', content: DESCRIPTION },
+      { name: 'og:image', content: '/form_header.jpg' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+    '~/assets/global.scss'
+  ],
+  plugins: [
+    '~/plugins/pwa-update.ts'
+  ],
+  components: true,
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/vuetify',
+    '@nuxtjs/composition-api/module',
+    ['nuxt-storm', { nested: true, alias: true }],
+  ],
+  modules: [
+    '@nuxtjs/pwa',
+  ],
+  pwa: {
+    manifest: {
+      name: TITLE,
+      short_name: TITLE,
+      description: DESCRIPTION,
+      lang: 'ru'
+    },
+    meta: {
+      name: TITLE,
+      description: DESCRIPTION
+    }
+  },
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  build: {},
+  generate: {
+    interval: 2000,
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script lang='ts'>
+import { defineComponent } from "@nuxtjs/composition-api"
+
+export default defineComponent({
+  setup() {
+
+  }
+})
+</script>
+
+<template>
+  <div>home</div>
+</template>
+
+<style lang='scss'>
+
+</style>
