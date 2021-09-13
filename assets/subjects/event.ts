de0cacb693f20b40ffcf6438ec519f6a8ceed944
Sugar_F0x <sugarfox.dev@gmail.com>
2021-09-14 00:36:02 +0300

    :art: Change Event management color to darker tone

--- assets/subjects/event.ts
+++ assets/subjects/event.ts
@@ -4,7 +4,7 @@ import { Event } from "~/assets/types"
 const EventSubject = new Subject({
   title: "Event-менеджмент",
   professor: "Шойгу Ксения Сергеевна",
-  color: "#bfef45",
+  color: "#043b0c",
 })
 
 const DAYS = ["23.10.2021", "30.10.2021", "06.11.2021", "13.11.2021", "20.11.2021", "27.11.2021"]
