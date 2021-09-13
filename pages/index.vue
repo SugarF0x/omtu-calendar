3118b892fc51581d6e79b775d611e6882c44778d
Sugar_F0x <sugarfox.dev@gmail.com>
2021-09-13 14:27:01 +0300

    :white_check_mark: Add month selection

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,45 @@
 <script lang='ts'>
 import { computed, defineComponent, ref } from '@nuxtjs/composition-api'
+import { addMonths, format, startOfMonth, subMonths } from 'date-fns'
 import { EVENTS } from '~/assets/subjects'
+
+interface CalendarRef {
+  next: () => void
+  prev: () => void
+}
 
 export default defineComponent({
   setup() {
-    const selectedGroup = ref(0)
+    const calendar = ref<CalendarRef | null>(null)
+    const value = ref('')
 
+    const selectedGroup = ref(0)
     const handleGroupSelection = (group: number) => {
       selectedGroup.value = group
+    }
+
+    const selectedMonth = ref(startOfMonth(new Date()))
+    const monthLocale = computed(() => format(selectedMonth.value, "MMMM"))
+    const handleMonthIncrease = () => {
+      selectedMonth.value = addMonths(selectedMonth.value, 1)
+      calendar.value?.next()
+    }
+    const handleMonthDecrease = () => {
+      selectedMonth.value = subMonths(selectedMonth.value, 1)
+      calendar.value?.prev()
     }
 
     const events = computed(() => EVENTS.filter(event => event.group === selectedGroup.value))
 
     return {
+      value,
+      calendar,
       events,
       selectedGroup,
       handleGroupSelection,
+      monthLocale,
+      handleMonthIncrease,
+      handleMonthDecrease,
     }
   }
 })
@@ -24,15 +48,26 @@ export default defineComponent({
 <template>
   <v-container class='wrapper'>
     <v-row class='fill-height'>
-      <v-col cols='12' md='2' class='d-flex justify-center'>
-        <v-radio-group v-model='selectedGroup' name='group'>
-          <v-radio v-for='n in 4' :key='n' :label='`${n}-я языковая`'></v-radio>
-        </v-radio-group>
+      <v-col cols='12' md='2'>
+        <v-card class='d-flex justify-center'>
+          <v-radio-group v-model='selectedGroup' name='group'>
+            <v-radio v-for='n in 4' :key='n' :label='`${n}-я языковая`'></v-radio>
+          </v-radio-group>
+        </v-card>
       </v-col>
       <v-col cols='12' md='8'>
-        <v-calendar class='calendar' :events='events' :weekdays='[1,2,3,4,5,6,0]' event-category='selectedGroup' event-overlap-mode='stack' />
+        <v-calendar ref="calendar" v-model='value' class='calendar' :events='events' :weekdays='[1,2,3,4,5,6,0]' event-category='selectedGroup' event-overlap-mode='stack' />
       </v-col>
-      <v-col cols='12' md='2'></v-col>
+      <v-col cols='12' md='2'>
+        <v-card>
+          <v-card-title class='justify-center'>{{ monthLocale }}</v-card-title>
+          <v-card-actions>
+            <v-btn @click='handleMonthDecrease'>&lt;</v-btn>
+            <v-spacer />
+            <v-btn @click='handleMonthIncrease'>&gt;</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
