43f63ac43d3d4826db62526e16dbbc1fca7b2090
Sugar_F0x <sugarfox.dev@gmail.com>
2021-09-14 00:34:12 +0300

    :hammer_and_wrench: Fix several type issues

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,13 @@
+<script lang='ts'>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+
+  }
+})
+</script>
+
 <template>
   <v-app>
     <v-main>
