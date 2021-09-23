82d2f1e299b51200387bec9f9e5d590b78cf93a3
Sugar_F0x <sugarfox.dev@gmail.com>
2021-09-23 18:49:34 +0300

    :construction: Setup navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,48 @@
 <script lang='ts'>
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from "@nuxtjs/composition-api";
 
 export default defineComponent({
   setup() {
+    const drawer = ref(false)
 
+    return {
+      drawer
+    }
   }
 })
 </script>
 
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" right fixed>
+      <v-layout class="fill-height" column>
+        <!-- Drawer content goes here -->
+      </v-layout>
+    </v-navigation-drawer>
+
+    <v-app-bar app class="navbar">
+      <router-link to="/" class="link">
+        <v-toolbar-title>
+          ОМТУ Календрь
+        </v-toolbar-title>
+      </router-link>
+
+      <v-spacer />
+
+      <v-app-bar-nav-icon @click="drawer = !drawer" >
+        <v-icon>mdi-cog</v-icon>
+      </v-app-bar-nav-icon>
+    </v-app-bar>
+
     <v-main>
       <nuxt />
     </v-main>
   </v-app>
 </template>
 
+<style lang="scss" scoped>
+.link {
+  color: white;
+  text-decoration: none;
+}
+</style>
