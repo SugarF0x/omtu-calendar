91c4ea3b254b2585a6808389c17fc96e6ce86b58
Alexey <sugarfox.dev@gmail.com>
2022-02-21 20:47:10 +0300

    feat(admin): add dev event item data panel

--- components/EventItem.vue
+++ components/EventItem.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
-import { defineComponent, PropType } from "@nuxtjs/composition-api"
+import { computed, defineComponent, PropType } from "@nuxtjs/composition-api"
 import { TransformedEvent } from "~/types"
+import { useAccessor } from "~/store"
 
 export default defineComponent({
   props: {
@@ -8,6 +9,15 @@ export default defineComponent({
       type: Object as PropType<TransformedEvent>,
       required: true,
     },
+  },
+  setup() {
+    const accessor = useAccessor()
+
+    const isDev = computed(() => accessor.env.isDev)
+
+    return {
+      isDev
+    }
   }
 })
 </script>
@@ -28,6 +38,19 @@ export default defineComponent({
       <div v-if="event.note" class="note">
         <h3>Примечание:</h3>
         <q>{{ event.note }}</q>
+      </div>
+
+      <div v-if="isDev" class="mt-5">
+        <h2>Debug</h2>
+        <v-expansion-panels accordion class="mt-3">
+          <v-expansion-panel class="grey darken-3">
+            <v-expansion-panel-header>EVENT_DATA</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <hr>
+              <pre class="mt-5">{{ JSON.stringify(event, null, 2) }}</pre>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </div>
     </v-card-text>
   </v-card>
