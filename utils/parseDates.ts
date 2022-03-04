c9985668c65fa1beb9223665d84715ce3fb74d9a
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-03-04 16:25:01 +0300

    fix(data): change event dates format

--- utils/parseDates.ts
+++ utils/parseDates.ts
@@ -1,13 +1,21 @@
 import { DAYS_FORMAT } from "~/consts"
 
+// separator between ranges with different months
+const MONTHS_SEPARATOR = '/'
+// separator between days-months-years
+const DATE_SEPARATOR = '.'
+// separator between days within single range
+const DAYS_SEPARATOR = ','
+
 export function parseDates(dates: string): string[] {
   const result: string[] = []
 
-  for (const months of dates.split(' - ')) {
-    const [days, month, year] = months.split('/')
-    for (const day of days.split(',')) {
+  const splitAndStrippedDates = dates.split(MONTHS_SEPARATOR).map(date => date.trim())
+  for (const months of splitAndStrippedDates) {
+    const [days, month, year] = months.split(DATE_SEPARATOR)
+    for (const day of days.split(DAYS_SEPARATOR)) {
       const date = `${day}.${month}.${year}`
-      if (date.length !== DAYS_FORMAT.length || isNaN(Number(`${day}${month}${year}`))) throw new Error('Неверный формат даты')
+      if (date.length !== DAYS_FORMAT.length || isNaN(Number(`${day}${month}${year}`))) throw new Error(`Неверный формат даты: ${date}`)
       result.push(`${day}.${month}.${year}`)
     }
   }
