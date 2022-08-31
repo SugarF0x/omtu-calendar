1470642640ca1eec2ad61bfe6fbe8474995f9293
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-08-31 19:35:46 +0300

    fix: crash on date input with stripped start padding

--- utils/parseDates.ts
+++ utils/parseDates.ts
@@ -13,8 +13,9 @@ export function parseDates(dates: string): string[] {
   const splitAndStrippedDates = dates.split(MONTHS_SEPARATOR).map(date => date.trim())
   for (const months of splitAndStrippedDates) {
     const [days, month, year] = months.split(DATE_SEPARATOR)
+
     for (const day of days.split(DAYS_SEPARATOR)) {
-      const date = `${day}.${month}.${year}`
+      const date = `${String(day).padStart(2, '0')}.${String(month).padStart(2, '0')}.${year}`
       if (date.length !== DAYS_FORMAT.length || isNaN(Number(`${day}${month}${year}`))) throw new Error(`Неверный формат даты: ${splitAndStrippedDates}, получено ${date}`)
       result.push(`${day}.${month}.${year}`)
     }
