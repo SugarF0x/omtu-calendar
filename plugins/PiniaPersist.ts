d7a7e6ced5b2670fb6cdc12d2d4e60b7501bf138
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-09-09 18:53:01 +0300

    globally disable persistence of loadings and errors

--- plugins/PiniaPersist.ts
+++ plugins/PiniaPersist.ts
@@ -1,6 +1,20 @@
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
+import { createPersistedState } from "pinia-plugin-persistedstate"
 import { defineNuxtPlugin } from "#app"
 
 export default defineNuxtPlugin(({ $pinia }) => {
-  $pinia.use(piniaPluginPersistedstate)
+  $pinia.use(createPersistedState({
+    serializer: {
+      deserialize: (store) => {
+        const parsed = JSON.parse(store)
+
+        for (const key of Object.keys(parsed)) {
+          if (['error', 'loading'].some(flag => key.toLowerCase().includes(flag))) delete parsed[key]
+        }
+
+        return parsed
+      },
+      serialize: JSON.stringify,
+    }
+  }))
 })
+
