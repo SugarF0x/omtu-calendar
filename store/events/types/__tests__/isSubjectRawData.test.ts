3726e3af6d34c7941bf602d3270c7b2f87d42652
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-09-12 21:18:03 +0300

    add isSubjectRawData tests

---
+++ store/events/types/__tests__/isSubjectRawData.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "vitest"
+import { isSubjectRawData } from "~/store/events"
+
+describe('isSubjectRawData', () => {
+  it('should validate proper input', () => {
+    expect(isSubjectRawData({
+      id: "",
+      title: "",
+      professor: "",
+      specs: "",
+      color: "",
+    })).toBeTruthy()
+  })
+
+  it.each([
+    {},
+    { id: "" },
+    { specs: "", color: "" },
+    {
+      id: 1,
+      title: "",
+      professor: "",
+      specs: "",
+      color: "",
+    }
+  ])('should invalidate malformed input', (input) => {
+    expect(isSubjectRawData(input)).not.toBeTruthy()
+  })
+})
