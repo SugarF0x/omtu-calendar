b79d730fa80428cd02ee30c611d3f47524c158f6
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-09-12 16:19:27 +0300

    add a few more units to sanitizeRow

--- utils/googleSheetParser/helpers/__tests__/sanitizeRow.test.ts
+++ utils/googleSheetParser/helpers/__tests__/sanitizeRow.test.ts
@@ -3,28 +3,61 @@ import { DirtyRow, Row } from "~/utils/googleSheetParser/types"
 import { sanitizeRow } from "~/utils/googleSheetParser/helpers"
 
 describe('sanitizeRow', () => {
-  it("should remove all nulls", () => {
-    const input: DirtyRow = {
-      c: [
-        { v: null },
-        null,
-        { v: 'value to be preserved A' },
-        null,
-        null,
-        { v: 'value to be preserved B' },
-        null,
-        { v: null },
-        { v: null },
-      ]
-    }
+  it.each<[DirtyRow, Row]>([
+    [
+      {
+        c: [
+          null,
+          null,
+          { v: 'one value among the nulls' },
+          null
+        ]
+      },
+      {
+        c: [
+          { v: 'one value among the nulls' }
+        ]
+      }
+    ],
 
-    const output: Row = {
-      c: [
-        { v: 'value to be preserved A' },
-        { v: 'value to be preserved B' },
-      ]
-    }
+    [
+      {
+        c: [
+          { v: null },
+          { v: 'a value among null values' },
+          { v: null },
+          { v: null }
+        ]
+      },
+      {
+        c: [
+          { v: 'a value among null values' },
+        ]
+      }
+    ],
 
+    [
+      {
+        c: [
+          { v: null },
+          null,
+          { v: 'value to be preserved A' },
+          null,
+          null,
+          { v: 'value to be preserved B' },
+          null,
+          { v: null },
+          { v: null },
+        ]
+      },
+      {
+        c: [
+          { v: 'value to be preserved A' },
+          { v: 'value to be preserved B' },
+        ]
+      }
+    ]
+  ])("should remove all nulls %#", (input, output) => {
     expect(sanitizeRow(input)).toEqual(output)
   })
 })
