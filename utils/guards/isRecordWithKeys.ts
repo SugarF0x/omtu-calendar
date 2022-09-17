0a77ba940a6250434ef5e3a2171a0887e6bca872
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-09-17 09:42:55 +0300

    fix type guard

--- utils/guards/isRecordWithKeys.ts
+++ utils/guards/isRecordWithKeys.ts
@@ -1,7 +1,7 @@
-import { isStringRecord } from "~/utils"
+import { isRecord } from "~/utils"
 
 export function isRecordWithKeys<Keys extends string>(object: unknown, keys: Array<Keys>): object is Record<Keys, unknown> {
-  if (!isStringRecord(object)) return false
+  if (!isRecord(object)) return false
   const objectKeys = Object.keys(object)
   return keys.every(key => objectKeys.includes(key))
 }
