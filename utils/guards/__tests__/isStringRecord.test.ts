28461ac689edce3f88d4bc1e5b27e3c0ded3bbaf
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-09-17 16:40:30 +0300

    add isStringRecord tests

---
+++ utils/guards/__tests__/isStringRecord.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest"
+import { isStringRecord } from "~/utils"
+
+describe('isStringRecord', () => {
+  it('should validate proper input', () => {
+    expect(isStringRecord({ foo: 'bar' })).toBeTruthy()
+  })
+
+  it("should validate empty object as well", () => {
+    expect(isStringRecord({})).toBeTruthy()
+  })
+
+  it.each<unknown>([
+    null,
+    undefined,
+    []
+  ])("should invalidate improper input", (input) => {
+    expect(isStringRecord(input)).not.toBeTruthy()
+  })
+})
