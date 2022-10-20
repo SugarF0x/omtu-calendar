947c49fe9ba2bf23ede1b0fe819288c59631accd
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-10-20 20:08:06 +0400

    fix hourly refetch early exit

--- hooks/useHourlyRefetch.ts
+++ hooks/useHourlyRefetch.ts
@@ -1,9 +1,8 @@
 import { onBeforeMount } from "#imports"
 import { differenceInHours } from "date-fns"
 
-export async function useHourlyRefetch(refetch: Function, updateTimestamp: string, dataRefetchInterval?: number) {
+export async function useHourlyRefetch(refetch: Function, updateTimestamp: string, dataRefetchInterval = 0) {
   onBeforeMount(() => {
-    if (dataRefetchInterval === undefined) return
-    if (differenceInHours(new Date(updateTimestamp), new Date()) >= dataRefetchInterval) refetch()
+    if (differenceInHours(new Date(), new Date(updateTimestamp)) >= dataRefetchInterval) refetch()
   })
 }
