9366012c60a1b82d7a8a902d71834d26b808a428
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-10-20 19:52:52 +0400

    add sheets data fetching mechanic

--- store/data/store.ts
+++ store/data/store.ts
@@ -1,12 +1,82 @@
 import { defineStore } from "pinia"
+import { ClassData, isClassData, isSpecialtyData, isSubjectData, SpecialtyData, SubjectData } from "./types"
+import { useConfigStore, useSettingsStore } from "~/store"
+import { useHourlyRefetch } from "~/hooks"
+import { fetchDoc } from "~/utils/googleSheetParser"
+import { isError } from "~/utils"
 
 export const useDataStore = defineStore(
   "data",
   () => {
-    const version = $ref(0)
+    let version = $ref(0)
 
-    return {
+    let allSubjects = $ref<Array<SubjectData[]>>([])
+    let allSpecialties = $ref<Array<SpecialtyData[]>>([])
+    let allClasses = $ref<Array<ClassData[]>>([])
 
+    let { course } = useSettingsStore()
+
+    const subjects = $computed<SubjectData[]>(() => allSubjects[course ?? -1] ?? [])
+    const specialties = $computed<SpecialtyData[]>(() => allSpecialties[course ?? -1] ?? [])
+    const classes = $computed<ClassData[]>(() => allClasses[course ?? -1] ?? [])
+
+    const { config } = useConfigStore()
+
+    let lastUpdateTimestamp = $ref(new Date(0).toISOString())
+    let isLoading = $ref(false)
+    let error = $ref<Error | null>(null)
+
+    async function fetchData(): Promise<void> {
+      if (!config?.sheetIds) return
+
+      error = null
+      isLoading = true
+
+      for (const [course, sheetId] of Object.entries(config.sheetIds)) {
+        {
+          const subjectsResponse = await fetchDoc<SubjectData>(sheetId, 'Предметы', isSubjectData)
+
+          if (isError(subjectsResponse)) {
+            error = subjectsResponse
+            break
+          }
+
+          allSubjects[Number(course)] = subjectsResponse
+        }
+
+        {
+          const specialtiesResponse = await fetchDoc<SpecialtyData>(sheetId, 'Специализации', isSpecialtyData)
+
+          if (isError(specialtiesResponse)) {
+            error = specialtiesResponse
+            break
+          }
+
+          allSpecialties[Number(course)] = specialtiesResponse
+        }
+
+        {
+          const classesResponse = await fetchDoc<ClassData>(sheetId, 'Предметы', isClassData)
+
+          if (isError(classesResponse)) {
+            error = classesResponse
+            break
+          }
+
+          allClasses[Number(course)] = classesResponse
+        }
+      }
+
+      isLoading = false
     }
+
+    useHourlyRefetch(fetchData, lastUpdateTimestamp, config?.dataRefetchInterval)
+
+    return $$({
+      subjects,
+      specialties,
+      classes,
+      fetchData
+    })
   }
 )
