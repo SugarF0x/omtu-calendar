a568a168dcbba0d0bf065ec6e5f9be67d01ba3de
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-10-23 16:05:28 +0300

    add config loading indicator

---
+++ components/ftue/Loader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useConfigStore } from "~/store"
+
+const { isLoading } = $(useConfigStore())
+</script>
+
+<template>
+  <div>
+    <main v-if="isLoading" class="loaderWrapper">
+      <progress class="progress w-56" />
+    </main>
+
+    <slot v-else />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.loaderWrapper {
+  @apply
+  h-screen
+  flex
+  justify-center
+  items-center
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -13,16 +13,18 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-      <div class="max-w-ld">
-        <h1 class="text-5xl font-bold">{{ lang.title }}</h1>
-        <div class="py-6">
-          <p v-for="locale in lang.paragraph" :key="locale">{{ locale }}</p>
-          <p><strong>{{ lang.paragraphNote }}</strong></p>
+  <ftue-loader>
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
+        <div class="max-w-ld">
+          <h1 class="text-5xl font-bold">{{ lang.title }}</h1>
+          <div class="py-6">
+            <p v-for="locale in lang.paragraph" :key="locale">{{ locale }}</p>
+            <p><strong>{{ lang.paragraphNote }}</strong></p>
+          </div>
+          <button class="btn btn-primary">Get Started</button>
         </div>
-        <button class="btn btn-primary">Get Started</button>
       </div>
     </div>
-  </div>
+  </ftue-loader>
 </template>
