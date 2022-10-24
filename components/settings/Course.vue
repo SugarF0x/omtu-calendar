b7e82e9eb14c2eaa521aa8b52d47f905623c99e9
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-10-24 13:42:05 +0300

    remove card style from course selector

--- components/settings/Course.vue
+++ components/settings/Course.vue
@@ -7,14 +7,10 @@ const { course } = $(useSettingsStore())
 
 <template>
   <div class="flex flex-col">
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title justify-center">{{ lang.title }}</h2>
-        <div class="btn-group justify-center">
-          <input class="btn" type="radio" name="options" data-title="1" :value="0" v-model="course" />
-          <input class="btn" type="radio" name="options" data-title="2" :value="1" v-model="course" />
-        </div>
-      </div>
+    <h2 class="card-title justify-center">{{ lang.title }}</h2>
+    <div class="btn-group justify-center">
+      <input class="btn" type="radio" name="options" data-title="1" :value="0" v-model="course" />
+      <input class="btn" type="radio" name="options" data-title="2" :value="1" v-model="course" />
     </div>
   </div>
 </template>
